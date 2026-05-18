1d867b68151cc64e4b98248d0394ac3c4e60de50
Asher <hoangthanhnhat0411@gmail.com>
2026-05-18 16:57:58 +0000

    fix: config docker

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -10,8 +10,7 @@
   wsl.defaultUser = "asher";
 
   # Bật Docker Engine native (không cần Docker Desktop)
-  virtualisation.docker.enable = true;
-  #Lazy load program
+  #  virtualisation.docker.enable = true;
   virtualisation.docker = {
     enable = true;
     enableOnBoot = false; 
